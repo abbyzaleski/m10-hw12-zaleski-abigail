1cc546fb279d65bdeba4970055f173d2a269db75
Abby Zaleski <azaleski1@ufl.edu>
2021-12-07 00:18:20 -0500

    created performMaintenance Method & called method

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ class Car {
     honk() {
         console.log('BEEP BEEP!')
     }
+    performMaintenance() {
+        setTimeout(function() {
+            console.log('maintenance complete');
+        }, 3000);
+    }
 }
 
 var mySweetRide = new Car('Pontiac', 'Fiero', 1988)
+
+mySweetRide.honk()
+mySweetRide.performMaintenance()

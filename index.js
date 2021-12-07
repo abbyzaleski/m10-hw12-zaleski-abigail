b84e5bd3bb831f63db5e4e5fc9c85fc3aa4eee81
Abby Zaleski <azaleski1@ufl.edu>
2021-12-06 23:48:56 -0500

    created constructor values for make model and year

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // YOUR CODE HERE
-class Car {}
+class Car {
+    constructor(make, model) {
+        this.make = make
+        this.model = model
+        this.year = 1998
+    }
+}
+
+var mySweetRide = new Car('Pontiac', 'Fiero', '1988')
